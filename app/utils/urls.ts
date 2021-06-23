450ceca38985b8b0c17b03824b738c80ec15737e
dave4506 <me@bydavidsun.com>
2021-06-22 21:33:48 -0500

    ADD shit

--- app/utils/urls.ts
+++ app/utils/urls.ts
@@ -3,12 +3,6 @@ import { BigNumber } from 'ethers';
 import { CHAIN_ID } from '../constants';
 import qs from 'query-string';
 
-export const getOpenSeaUrl = (tokenId: string) => {
-  return `https://${CHAIN_ID === 1 ? '' : 'testnets.'}opensea.io/assets/${
-    deployments[CHAIN_ID].erc1155
-  }/${BigNumber.from(tokenId).toString()}`;
-};
-
 export const getOpenSeaAccountUrl = (address: string) => {
   return `https://${
     CHAIN_ID === 1 ? '' : 'testnets.'
