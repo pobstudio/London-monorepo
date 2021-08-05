c494ab06453fc03fb887fde2e8e2dabef3e3a448
dave4506 <me@bydavidsun.com>
2021-08-05 05:48:40 -0700

    FIX

--- protocol/tasks/deploy-nft.ts
+++ protocol/tasks/deploy-nft.ts
@@ -39,42 +39,42 @@ task('deploy-nft', 'Deploys $LONDON ', async (args, hre) => {
 
   // deploy LondonGift
   const LondonGift = await hre.ethers.getContractFactory('LondonGift');
-  // const londonGift = await LondonGift.attach(
-  //   deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].gift,
-  // );
-  const londonGift = (await LondonGift.deploy(
-    name,
-    symbol,
-    erc20.address,
-    mintPrice,
-    maxSupply,
-    provenance,
-  )) as LondonGift;
-  await londonGift.deployed();
-  console.log('LondonGift deployed to:', londonGift.address);
+  const londonGift = await LondonGift.attach(
+    deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].gift,
+  );
+  // const londonGift = (await LondonGift.deploy(
+  //   name,
+  //   symbol,
+  //   erc20.address,
+  //   mintPrice,
+  //   maxSupply,
+  //   provenance,
+  // )) as LondonGift;
+  // await londonGift.deployed();
+  // console.log('LondonGift deployed to:', londonGift.address);
 
   console.log('wiring metadata');
-  await londonGift.setTreasury(
-    deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].multisig,
-  );
-  await londonGift.setContractURI(
-    `ipfs://${
-      deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].contractURI
-    }/`,
-  );
-  // await londonGift.setBaseMetadataURI(
-  //   `ipfs://${
-  //     deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].baseTokenURI
-  //   }/`,
-  // );
-  await londonGift.setMintStartAtBlockNum(blockNumberUpTo);
-  await londonGift.setUnlockStartAtBlockNum(
-    blockNumberUpTo + BLOCKS_PER_24_HRS * 0.5,
-  );
-  await londonGift.setRevealStartAtBlockNum(
-    blockNumberUpTo + BLOCKS_PER_24_HRS * 1.5,
-  );
-  // await londonGift.transferOwnership(
+  // await londonGift.setTreasury(
   //   deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].multisig,
   // );
+  // await londonGift.setContractURI(
+  //   `ipfs://${
+  //     deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].contractURI
+  //   }/`,
+  // );
+  await londonGift.setBaseMetadataURI(
+    `ipfs://${
+      deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].baseTokenURI
+    }/`,
+  );
+  // await londonGift.setMintStartAtBlockNum(blockNumberUpTo);
+  // await londonGift.setUnlockStartAtBlockNum(
+  //   blockNumberUpTo + BLOCKS_PER_24_HRS * 0.5,
+  // );
+  // await londonGift.setRevealStartAtBlockNum(
+  //   blockNumberUpTo + BLOCKS_PER_24_HRS * 1.5,
+  // );
+  await londonGift.transferOwnership(
+    deployments[NETWORK_NAME_CHAIN_ID[hre.network.name]].multisig,
+  );
 });
