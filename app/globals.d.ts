697d50eae9a8d83993c29c296c71daa627080608
dave4506 <me@bydavidsun.com>
2021-12-06 13:58:29 -0600

    TRY this

--- app/globals.d.ts
+++ app/globals.d.ts
@@ -2,4 +2,7 @@ declare interface Window {
   ethereum: any;
 }
 
+declare module 'convert-svg-to-png ';
 declare module 'canvas-sketch';
+declare module 'png-js';
+declare module 'gif-encoder';
--- sketches/src/globals.d.ts
+++ sketches/src/globals.d.ts
@@ -2,4 +2,3 @@ declare module 'convert-svg-to-png ';
 declare module 'canvas-sketch';
 declare module 'png-js';
 declare module 'gif-encoder';
-declare module 'convert-svg-to-png';
